7405074eb579c7796c1c2a25f359e18dffc4b801
shabloel <shabloel@outlook.com>
2021-12-13 13:33:17 +0100

    first commit

---
+++ src/main/java/com/appsdeveloperblog/clients/sociallogin/socialloginwebclient/SocialLoginWebclientApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.clients.sociallogin.socialloginwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginWebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLoginWebclientApplication.class, args);
+    }
+
+}
